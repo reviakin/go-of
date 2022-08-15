5871ca11ea05913480d5d2adafd1af08d8908ce6
reviakin <reviakindi@gmail.com>
2022-08-15 17:13:55 +0200

    added gladys

--- hello/hello.go
+++ hello/hello.go
@@ -8,10 +8,17 @@ import (
 )
 
 func main() {
+    // set props of the predefined logger,
+    // including log entry prefix and a flag 
+    // to disable printing the time, source title
+    // and line number.
     log.SetPrefix("greetings: ")
     log.SetFlags(0)
 
-    message, err := greetings.Hello("")
+    // request a greeting message
+    message, err := greetings.Hello("Gladys")
+    // if an error was returned, print it to the console
+    // and exit program
     if err != nil {
       log.Fatal(err)
     }
